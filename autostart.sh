9ab38903d1caaa78f6b1c9d7aaba41eab1e61bbb
Lynianore <39530409+Lynianore@users.noreply.github.com>
2023-12-11 20:31:28 +0000

    Add files via upload

---
+++ autostart.sh
@@ -0,0 +1,4 @@
+xrandr --output eDP-1 --auto --output HDMI-1-0 --auto --primary --left-of eDP-1
+dwmblocks
+picom
+syncthing
---
+++ dwmblocks/blocks.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"",	"sb-forecast",	18000,	5},
+	{"",	"sb-price xmr Monero 🔒 24",			9000,	24},
+	{"",	"sb-battery",	5,	3},
+	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
